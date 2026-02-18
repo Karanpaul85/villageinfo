bc7877bf1d31e88a92a2aa751a9344f78dd76474
Karanpaul85 <karanpaul85@gmail.com>
2026-02-18 19:28:19 +0530

    create all pages

--- app/[state]/[district]/[tehsil]/[village]/page.tsx
+++ app/[state]/[district]/[tehsil]/[village]/page.tsx
@@ -554,7 +554,7 @@ export default async function VillagePage({ params }: Props) {
         </div>
       </div>
       {/** all state link */}
-      <div className="flex items-center mt-8 text-sm gap-2 w-full md:w-2/3 border border-gray-200 rounded-lg p-4 shadow-[0_6px_18px_rgba(15,23,42,0.05)]">
+      <div className="flex items-center mt-8 text-sm gap-2 w-full border border-gray-200 rounded-lg p-4 shadow-[0_6px_18px_rgba(15,23,42,0.05)]">
         Explore more:{" "}
         <Link
           href={`/${villagesData?.state_slug}/${villagesData?.district_slug}/${villagesData?.block_slug}/`}
