c24d340ec737cbdf6bc826189ddb9498ad308c96
Karanpaul85 <karanpaul85@gmail.com>
2026-02-19 21:09:41 +0530

    add header and footer in layout page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
 import Link from "next/link";
 import { getStates } from "@/utils/common";
+import Image from "next/image";
 
 export default async function Home() {
   const states = await getStates();
   return (
-    <div className="flex">
-      <main className="flex">
-        {states.length === 0 ? (
-          <p>No states found.</p>
-        ) : (
-          states.map(
+    <main className="flex w-full md:max-w-275 mx-auto p-4 flex-wrap">
+      {states.length === 0 ? (
+        <p>No states found.</p>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
+          {states.map(
             (state: { _id: string; state_slug: string; state: string }) => (
-              <div key={state._id} className="flex">
-                <Link href={`/${state.state_slug}`}>{state.state}</Link>
-              </div>
+              <Link
+                href={`/${state.state_slug}`}
+                key={state._id}
+                className="flex items-center border border-gray-300 rounded-lg p-4 hover:bg-gray-100 transition"
+              >
+                <Image
+                  className="mr-2"
+                  src="/icons/mapPin.svg"
+                  alt={state.state}
+                  width={20}
+                  height={20}
+                />
+                {state.state}
+              </Link>
             ),
-          )
-        )}
-      </main>
-    </div>
+          )}
+        </div>
+      )}
+    </main>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-sky-950 text-white p-4 sticky top-0 z-50 shadow-indigo-200 shadow-md">
+      <div className="flex w-full md:max-w-275 m-auto flex-wrap">
+        <h1 className="text-2xl font-bold">Village Info</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
